3f5833e47d1a0d5b93d93be1ca9055f271396677
zahee <F2021266557@umt.edu.pk>
2023-07-16 00:26:27 +0500

    Add project files.

---
+++ YTServer/ServerMaincpp.cpp
@@ -0,0 +1,15 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <conio.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+using namespace std;
+
+int main()
+{
+
+	getch();
+	return 0;
+}
