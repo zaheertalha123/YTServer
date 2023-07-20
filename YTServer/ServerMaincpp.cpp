ff66264f7cc1548e34447ec52530af5da3b27de8
zahee <F2021266557@umt.edu.pk>
2023-07-20 17:40:17 +0500

    WSAStartup/Cleanup

--- YTServer/ServerMaincpp.cpp
+++ YTServer/ServerMaincpp.cpp
@@ -5,11 +5,25 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
+#pragma comment (lib, "ws2_32.lib")
+
 using namespace std;
 
 int main()
 {
+	WSAData wsadata;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+	if (result != 0)
+	{
+		cout << "WSAStartup Failed With Error: " << result << endl;
+	}
 
-	getch();
+	cout << "Status: " << wsadata.szSystemStatus << endl;
+
+	WSACleanup();
+
+	cout << "Completed" << endl;
+	(void)getch();
 	return 0;
 }
+
